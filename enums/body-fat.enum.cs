282a9baa4eb79dc1ddcdf454b402889eaca43e43
snirsh <s.sharristh@gmail.com>
2022-03-05 17:34:30 +0200

    body fat and body muscles added

---
+++ enums/body-fat.enum.cs
@@ -0,0 +1,8 @@
+namespace enums
+{
+    public enum BodyFatEnum
+    {
+        Thin,
+        Fat
+    }
+}
---
+++ enums/body-muscles.enum.cs
@@ -0,0 +1,8 @@
+namespace enums
+{
+    public enum BodyMusclesEnum
+    {
+        Wimp,
+        Mascular
+    }
+}
