6fc375d22eff58fa71b98290850fd6336e80f062
snirsh <s.sharristh@gmail.com>
2022-03-09 08:55:32 +0200

    better code style

--- consts/natural-eye-color.const.cs
+++ consts/natural-eye-color.const.cs
@@ -1,19 +1,17 @@
 using System.Collections.Generic;
+using consts;
+using DefaultNamespace;
 using UnityEngine;
 
 namespace DefaultNamespace
 {
     public static class NaturalEyeColors
     {
-        private static readonly List<string> EyeColors = new List<string>
+        private static List<string> EyeColors { get; } = new()
         {
             "b1b1b1", "070504", "341c0d", "62422e", "914329", "cd622b", "ad7b41", "e4b877"
         };
         
-        public static List<string> GetOptions()
-        {
-            return EyeColors;
-        }
         public static Color GetRandom()
         {
             var index = Random.Range(0, (int) (EyeColors.Count - 1));
