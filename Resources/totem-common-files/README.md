3ce49d501cf3a0a0ed353ee9511042c6b14e68a1
sergey.v <voropserg@meta.ua>
2022-12-01 18:11:44 +0200

    Version bump, non-git filters

---
+++ Resources/totem-common-files/README.md
@@ -0,0 +1,2 @@
+# totem-common-files
+This repository contains the common static files, like filters and ABI of the smart contracts, that are required for several Totem system's components.
---
+++ Resources/totem-common-files/index.js
@@ -0,0 +1,8 @@
+exports.itemFilterJson = require('./filters/totem-item.json')
+exports.avatarFilterJson = require('./filters/totem-avatar.json')
+exports.assets_abi = require('./ABI/assets_abi.json')
+exports.game_directory_abi = require('./ABI/json/TotemGameDirectory.json')
+exports.legacy_abi = require('./ABI/json/TotemLegacy.json')
+exports.totemAvatarDreadstoneKeepFilterJson = require('./filters/totem-avatar-dreadstone-keep.json')
+exports.monkVsRobotsAvatarFilterJson = require('./filters/monk-vs-robots-avatar.json')
+
